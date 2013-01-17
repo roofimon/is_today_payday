7b3931219b245110018f19ea4bbf7caeb62cfe92
Twin Panichsombat <roofimon@gmail.com>
2013-01-17 10:23:28 +0700

    Refactor payday_spec by extracting warning and notification messages

--- spec/payday_spec.rb
+++ spec/payday_spec.rb
@@ -5,6 +5,9 @@ require 'date'
 describe PayMaster do
   let(:pay_master){ PayMaster.new }
   let(:current_date){ Date.today }
+  let(:early_payment_message) {"An actual payday is weekend so your Pay Master will pay you sooner and your payday is"}
+  let(:weekend_payment_warning) {"Stupid dude, who's gonna pay you on weekend"}
+
   describe "ask for payday" do
     context "when payday is weekday" do
       it "should tell me that my payday is 'Tuesday, 26-02-2013" do
@@ -13,15 +16,13 @@ describe PayMaster do
       end
     end
     context "when payday is weekend" do
-      it "should tell me that
-        'An actual payday is weekend so your Pay Master will pay you sooner and your payday is 'Friday, that_date-current_month-current_year'" do
+      it "should tell me an early_payment_message with 'Friday, that_date-current_month-current_year'" do
           Date.stub!(:today).and_return(Date.rfc2822('Sun, 26 May 2013 00:00:00 +0000'))
-          pay_master.when_is_my_payday.should eq("An actual payday is weekend so your Pay Master will pay you sooner and your payday is Friday, 24-05-2013")
+          pay_master.when_is_my_payday.should eq( "#{early_payment_message} Friday, 24-05-2013")
       end
-      it "should tell me that
-        'An actual payday is weekend so your Pay Master will pay you sooner and your payday is 'Friday, that_date-current_month-current_year'" do
+      it "should tell me an early_payment_message with 'Friday, that_date-current_month-current_year'" do
         Date.stub!(:today).and_return(Date.rfc2822('Sat, 26 Oct 2013 00:00:00 +0000'))
-        pay_master.when_is_my_payday.should eq("An actual payday is weekend so your Pay Master will pay you sooner and your payday is Friday, 25-10-2013")
+        pay_master.when_is_my_payday.should eq("#{early_payment_message} Friday, 25-10-2013")
       end
     end
   end
@@ -36,12 +37,12 @@ describe PayMaster do
   describe "today is weekend" do
     context "saturday" do
       it "should tell me that 'Stupid dude, who's gonna pay you on weekend'" do
-        pay_master.today_is_payday(Date.rfc2822('Sat, 12 Jan 2013 00:00:00 +0000')).should eq("Stupid dude, who's gonna pay you on weekend")
+        pay_master.today_is_payday(Date.rfc2822('Sat, 12 Jan 2013 00:00:00 +0000')).should eq(weekend_payment_warning)
       end
     end
     context "sunday" do
       it "should tell me that 'Stupid dude, who's gonna pay you on weekend'" do
-        pay_master.today_is_payday(Date.rfc2822('Sun, 13 Jan 2013 00:00:00 +0000')).should eq("Stupid dude, who's gonna pay you on weekend")
+        pay_master.today_is_payday(Date.rfc2822('Sun, 13 Jan 2013 00:00:00 +0000')).should eq(weekend_payment_warning)
       end
     end
   end
